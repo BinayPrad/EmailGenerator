4fbaccf8d399cf84f5fdaf91f0713a87f298bb57
BinayPrad <115228155+BinayPrad@users.noreply.github.com>
2023-07-19 17:23:23 +0530

    Update emailapp.py

--- emailapp.py
+++ emailapp.py
@@ -1,44 +1,23 @@
-import openai
 import streamlit as st
-from ml_backend import ml_backend
+import openai
+question=st.text_area("Input the Email context Here")
+button=st.button("Generate")
+def response1(ques):
+    openai.api_type = "azure"
+openai.api_base = "https://hack2023d1.openai.azure.com/"
+openai.api_version = "2022-12-01"
+openai.api_key = st.secrets("OPENAI_API_KEY")
 
-st.title("Automated Email Generator App")
+response = openai.Completion.create(
+  engine="hack2023d1gpt35",
+  prompt="Write a product launch email for new AI-powered headphones that are priced at $79.99 and available at Best Buy, Target and Amazon.com. The target audience is tech-savvy music lovers and the tone is friendly and exciting.\n\n1. What should be the subject line of the email?  \n2. What should be the body of the email?",
+  temperature=1,
+  max_tokens=350,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop=None)
 
-coder = '<h1 style="font-family:Times-New-Roman; color:Red ; font-size: 20px;">by Binay Pradhan</h1>'
-st.markdown(coder,unsafe_allow_html=True)
-
-st.title("Generate Email")
-
-backend = ml_backend()
-
-with st.form(key="form"):
-    prompt = st.text_input("Describe the Kind of Email you want to be written.")
-    st.text(f"(Example: Write me a professional sounding email to my boss)")
-
-    start = st.text_input("Begin writing the first few or several words of your email:")
-
-    slider = st.slider("How many characters do you want your email to be? ", min_value=64, max_value=750)
-    st.text("(A typical email is usually 100-500 characters)")
-
-    submit_button = st.form_submit_button(label='Generate Email')
-
-    if submit_button:
-        with st.spinner("Generating Email..."):
-            output = backend.generate_email(prompt, start)
-        st.markdown("# Email Output:")
-        st.subheader(start + output)
-
-        st.markdown("____")
-
-        st.subheader("You can press the Generate Email Button again if you're unhappy with the model's output")
-
-        st.markdown("____")
-
-        st.markdown("# Send Your Email Now")
-
-        
-        # st.subheader("Otherwise:")
-        # st.text(start + output)
-        url = "https://mail.google.com/mail/?view=cm&fs=1&to=&su=&body=" + backend.replace_spaces_with_pluses(start + output)
-
-        st.markdown("[Click me to send the email]({})".format(url))
+if question and button:
+        answer=response1(question)
+        st.code(answer)
