673951d8b1cd65225fc8758dba42b547625b19ea
BinayPrad <115228155+BinayPrad@users.noreply.github.com>
2023-07-20 00:21:30 +0530

    Update emailapp.py

--- emailapp.py
+++ emailapp.py
@@ -2,22 +2,25 @@ import streamlit as st
 import openai
 question=st.text_area("Input the Email context Here")
 button=st.button("Generate")
-def response1(ques):
-    openai.api_type = "azure"
-openai.api_base = "https://hack2023d1.openai.azure.com/"
-openai.api_version = "2022-12-01"
+
 openai.api_key = st.secrets("OPENAI_API_KEY")
+def response1(self, userPrompt ="Write me a professionally sounding email", start="Dear"):
 
-response = openai.Completion.create(
-  engine="hack2023d1gpt35",
-  prompt="Write a product launch email for new AI-powered headphones that are priced at $79.99 and available at Best Buy, Target and Amazon.com. The target audience is tech-savvy music lovers and the tone is friendly and exciting.\n\n1. What should be the subject line of the email?  \n2. What should be the body of the email?",
-  temperature=1,
-  max_tokens=350,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0,
-  stop=None)
+        response = openai.Completion.create(
+        engine="davinci",
+        prompt=userPrompt + "\n\n" + start,
+        temperature=0.71,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0.36,
+        presence_penalty=0.75
+        )
+        return response.get("choices")[0]['text']
 
-if question and button:
-        answer=response1(question)
-        st.code(answer)
+    def replace_spaces_with_pluses(self, sample):
+        """Returns a string with each space being replaced with a plus so the email hyperlink can be formatted properly"""
+        changed = list(sample)
+        for i, c in enumerate(changed):
+            if(c == ' ' or c =='  ' or c =='   ' or c=='\n' or c=='\n\n'):
+                changed[i] = '+'
+        return ''.join(changed)
